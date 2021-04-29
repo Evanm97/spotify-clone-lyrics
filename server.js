a0358cb8b551ddbd6090b04725e9041f072a4263
Evan <evanmurray2012@gmail.com>
2021-04-29 19:59:45 +0100

    initial commit

--- server.js
+++ server.js
@@ -69,11 +69,11 @@ app.get("/lyrics", async (req, res) => {
 // app.use(express.static(__dirname + '/client/build'));
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
+  
+  app.use(express.static(path.resolve(__dirname, 'build')));
+
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
